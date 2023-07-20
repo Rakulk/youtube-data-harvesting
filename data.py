6f4a80385e7b0baf914a96f0a4bf7683d515cd79
Rakulk <128506640+Rakulk@users.noreply.github.com>
2023-07-20 13:18:49 +0530

    data.py

--- data.py
+++ data.py
@@ -428,7 +428,7 @@ if choice=="Spicy Questions":
         host="localhost",
         user="root",
         password="Rakul@9360721393",
-        databse='youtubedata',
+        database='youtubedata',
         autocommit=True)
         
         mycursor=mydb.cursor(buffered=True)
